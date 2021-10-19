8cc14808051b712d60256cf0c2458859cfc2ee76
Justin Frank <justinpfrank@protonmail.com>
2021-10-19 18:03:56 -0400

    Refactor function types to try and add inductive data types

--- lang-gen.rkt
+++ lang-gen.rkt
@@ -13,11 +13,7 @@
 ;;           (... generate an expression with x is eof ...)))
 ;;   - could add a "narrow" form that takes a variable in the environment that is a
 ;;     TOr and scrutinizes it and narrows the types in the sub-terms.
-;; lists, pairs, strings, vectors
-
-;; read-byte
-;; peek-byte
-;; write-byte
+;; strings, vectors
 
 (provide build-gen:expr
          build-env
@@ -30,8 +26,8 @@
          (struct-out TChar)
          (struct-out TEOF)
          (struct-out TFun)
-         (struct-out TCaseFun)
-         T->)
+         #;(struct-out TCaseFun)
+         #;T->)
 
 ;; Type Structs
 (struct TAny () #:prefab)
@@ -51,13 +47,15 @@
 
 (struct TBox (type) #:prefab)
 
-(struct TFun (domain optional rest? codomain) #:prefab
+(struct TFun (dom-gen) #:prefab)
+
+#;(struct TFun (domain optional rest? codomain) #:prefab
   #:extra-constructor-name T->*)
 
 ;; types must all be TFuns
-(struct TCaseFun (types) #:prefab)
+#;(struct TCaseFun (types) #:prefab)
 
-(define-syntax-rule (T-> domain ... codomain)
+#;(define-syntax-rule (T-> domain ... codomain)
   (TFun (list domain ...) '() #f codomain))
 
 (define (quotable? t)
@@ -77,7 +75,7 @@
     ;; Reflexivity
     [(t t) #t]
 
-    ;; Short-circuit these cases after reflexivity and or.
+    ;; Short-circuit these cases after reflexivity.
     [(_ (TAny)) #f]
     [((TBot) _) #f]
 
@@ -87,13 +85,14 @@
     [((TPair l1 r1) (TPair l2 r2)) (and (type-subsumes? l1 l2)
                                         (type-subsumes? r1 r2))]
     [((TList t1) (TList t2)) (type-subsumes? t1 t2)]
-    ;; Not sure if this is needed
     [((TList t) (TEmpty)) #t]
 
     [((TList t1) (TPair l r)) (and (type-subsumes? t1 l)
                                    (type-subsumes? (TList t1) r))]
 
-    [((TFun d1 d1-opt d1-rest? c1) (TFun d2 d2-opt d2-rest? c2))
+    [((TBox t1) (TBox t2)) (type-subsumes? t1 t2)]
+
+    #;[((TFun d1 d1-opt d1-rest? c1) (TFun d2 d2-opt d2-rest? c2))
      (and (type-subsumes? c1 c2)
           (domain-subsumes? t2 t1))]
     
@@ -134,8 +133,6 @@
 (define (gen:val k type)
   (match type
     [(TAny) (gen:bind (gen:base-type k) (λ (t) (gen:val k t)))]
-    ;[(TQuotable) (gen:bind (gen:base-type k quotable?) (λ (t) (gen:val k t)))]
-    ;[(TOr l r) (gen:bind gen:boolean (λ (side) (gen:val k (if side l r))))]
     [(TNat) (gen:map gen:real (λ (x) (exact-floor (* (- (/ 1 x) 1) 2))))]
     [(TInt) (gen:let ([n (gen:val k (TNat))]
                       [neg? gen:boolean])
@@ -159,25 +156,24 @@
                    (λ (t) (and (base-type? k t)
                                (type-subsumes? type t)))
                    env))])
-    (if (empty? env-candidates)
-        (gen:val k type)
-        (gen:choice (gen:val k type)
-                    (gen:one-of env-candidates)))))
+    (gen:frequency
+     (list (cons 1 (gen:val k type))
+           (cons (length env-candidates)
+                 (gen:delay (gen:one-of env-candidates)))))))
 
-(define (gen:function-domain tf)
-  (match-let ([(TFun domain opt rest? _) tf])
-    (cond
-      [(and (empty? opt) (false? rest?)) (gen:const domain)]
-      [(false? rest?) (gen:let ([m (gen:integer-in 0 (length opt))])
-                               (append domain (take opt m)))]
-      [else
-       (gen:let ([n (gen:map gen:natural (compose exact-ceiling sqrt))]
-                 [m (gen:integer-in 0 (length opt))])
-                (append domain
-                        (take opt (min (+ n m) (length opt)))
-                        (make-list (max 0 (- (+ n m) (length opt))) rest?)))])))
+(define (gen:function-domain req opt rest?)
+  (cond
+    [(and (empty? opt) (false? rest?)) (gen:const req)]
+    [(false? rest?) (gen:let ([m (gen:integer-in 0 (length opt))])
+                             (append req (take opt m)))]
+    [else
+     (gen:let ([n (gen:map gen:natural (compose exact-ceiling sqrt))]
+               [m (gen:integer-in 0 (length opt))])
+              (append req
+                      (take opt (min (+ n m) (length opt)))
+                      (make-list (max 0 (- (+ n m) (length opt))) rest?)))]))
 
-(define (funs-with-codomain codomain env)
+#;(define (funs-with-codomain codomain env)
   (foldl-env
    (λ (id type acc)
      (cond
@@ -195,13 +191,19 @@
    env))
 
 (define (gen:app k env codomain)
-  (let ([candidates (funs-with-codomain codomain env)])
+  (let ([candidates (foldr (λ (m acc)
+                             (let ([dom-gen? ((TFun-dom-gen (cdr m)) codomain)])
+                               (if dom-gen?
+                                   (cons (cons (car m) dom-gen?) acc)
+                                   acc)))
+                           '()
+                           (filter-env TFun? env))])
     (gen:sized
      (λ (size)
        (if (empty? candidates)
            (gen:resize ((knot-expr k) env codomain) (quotient size 2))
            (gen:let ([m (gen:one-of candidates)]
-                     [dom (gen:function-domain (cdr m))]
+                     [dom (cdr m)]
                      [es (gen:resize
                           (gen:tuple* (map (curry (knot-expr k) env) dom))
                           (quotient size (add1 (length dom))))])
@@ -330,10 +332,6 @@
                        #f))
                  ,e2)))))
 
-;; builds and let-binds an inductive data type
-(define (gen:inductive k env type)
-  'TODO)
-
 (define (type-gen t)
   (match t
     ['integers (λ (k) (gen:one-of (list (TInt) (TNat))))]
@@ -382,43 +380,69 @@
     ['begin gen:begin]
     ['read  gen:read-op]))
 
+(define (basic-fun codomain . domain)
+  (λ (co)
+    (if (type-subsumes? co codomain)
+        (gen:const domain)
+        #f)))
+
+(define (full-fun codomain req opt rest?)
+  (λ (co)
+    (if (type-subsumes? co codomain)
+        (gen:function-domain req opt rest?)
+        #f)))
+
+(define (case-fun cs)
+  (λ (co)
+    (let loop ([cs cs])
+      (match cs
+        ['() #f]
+        [(cons f cs)
+         (let ([gen? (f co)])
+           (or gen? (loop cs)))]))))
+
 (define (build-env ops)
   (define (lookup-op op)
     (match op
-      ['add1          (cons 'add1 (TCaseFun
-                                   (list (T-> (TNat) (TNat))
-                                         (T-> (TInt) (TInt)))))]
-      ['sub1          (cons 'sub1 (T-> (TInt) (TInt)))]
-      ['abs           (cons 'abs (T-> (TInt) (TNat)))]
-      ['not           (cons 'not (T-> (TAny) (TBool)))]
-      ['zero?         (cons 'zero? (T-> (TInt) (TBool)))]
-      ['integer?      (cons 'integer? (T-> (TAny) (TBool)))]
-      ['boolean?      (cons 'boolean? (T-> (TAny) (TBool)))]
-      ['char?         (cons 'char? (T-> (TAny) (TBool)))]
-      ['eof-object?   (cons 'eof-object? (T-> (TAny) (TBool)))]
-      ['unary-        (cons '- (T-> (TInt) (TInt)))]
-      ['binary-       (cons '- (T-> (TInt) (TInt) (TInt)))]
-      ['un/binary-    (cons '- (T->* (list (TInt)) (list (TInt)) #f (TInt)))]
-      ['-             (cons '- (T->* (list (TInt)) '() (TInt) (TInt)))]
-      ['binary+       (cons '+ (TCaseFun
-                                (list (T-> (TNat) (TNat) (TNat))
-                                      (T-> (TInt) (TInt) (TInt)))))]
-      ['+             (cons '+ (TCaseFun
-                                (list (T->* '() '() (TNat) (TNat))
-                                      (T->* '() '() (TInt) (TInt)))))]
-      ['integer->char (cons 'integer->char (T-> (TNat) (TChar)))]
-      ['char->integer (cons 'char->integer (T-> (TChar) (TNat)))]
+      ['add1          (cons 'add1 (TFun (case-fun (list (basic-fun (TInt) (TInt))
+                                                        (basic-fun (TNat) (TNat))))))]
+      ['sub1          (cons 'sub1 (TFun (basic-fun (TInt) (TInt))))]
+      ['abs           (cons 'abs (TFun (basic-fun (TNat) (TInt))))]
+      ['not           (cons 'not (TFun (basic-fun (TBool) (TAny))))]
+      ['zero?         (cons 'zero? (TFun (basic-fun (TBool) (TInt))))]
+      ['integer?      (cons 'integer? (TFun (basic-fun (TBool) (TAny))))]
+      ['boolean?      (cons 'boolean? (TFun (basic-fun (TBool) (TAny))))]
+      ['char?         (cons 'char? (TFun (basic-fun (TBool) (TAny))))]
+      ['eof-object?   (cons 'eof-object? (TFun (basic-fun (TBool) (TAny))))]
+      ['unary-        (cons '- (TFun (basic-fun (TInt) (TInt))))]
+      ['binary-       (cons '- (TFun (basic-fun (TInt) (TInt) (TInt))))]
+      ['un/binary-    (cons '- (TFun (full-fun (TInt) (list (TInt)) (list (TInt)) #f)))]
+      ['-             (cons '- (TFun (full-fun (TInt) (list (TInt)) '() (TInt))))]
+      ['binary+       (cons '+ (TFun (case-fun (list (basic-fun (TInt) (TInt))
+                                                     (basic-fun (TNat) (TNat))))))]
+      ['+             (cons '+ (TFun (case-fun (list (full-fun (TInt) '() '() (TInt))
+                                                     (full-fun (TNat) '() '() (TNat))))))]
+      ['integer->char (cons 'integer->char (TFun (basic-fun (TChar) (TNat))))]
+      ['char->integer (cons 'char->integer (TFun (basic-fun (TNat) (TChar))))]
 
-      ;; TODO: these need forall types
-      ['cons          (cons 'cons (T-> 'A 'B (TPair 'A 'B)))]
-      ['car           (cons 'car (T-> (TPair 'A 'B) 'A))]
-      ['cdr           (cons 'cdr (T-> (TPair 'A 'B) 'B))]
-      ['cons?         (cons 'cons? (T-> (TAny) (TBool)))]
-      ['empty?        (cons 'empty? (T-> (TAny) (TBool)))]
+      ['cons          (cons 'cons (TFun (λ (co)
+                                          (cond
+                                            [(TAny? co) (gen:const (list (TAny) (TAny)))]
+                                            [(TPair? co) (gen:const (list (TPair-left co)
+                                                                          (TPair-right co)))]
+                                            [(TList? co) (gen:const (list (TList-type co) co))]
+                                            [else #f]))))]
+      ['car           (cons 'car (TFun (λ (co) (gen:const (list (TPair co (TAny)))))))]
+      ['cdr           (cons 'cdr (TFun (λ (co) (gen:const (list (TPair (TAny) co))))))]
+      ['cons?         (cons 'cons? (TFun (basic-fun (TBool) (TAny))))]
+      ['empty?        (cons 'empty? (TFun (basic-fun (TBool) (TAny))))]
 
-      ['box           (cons 'box (T-> 'A (TBox 'A)))]
-      ['unbox         (cons 'unbox (T-> (TBox 'A) 'A))]
-      ['box?          (cons 'box? (T-> (TAny) (TBool)))]))
+      ['box           (cons 'box (TFun (λ (co)
+                                         (if (TBox? co)
+                                             (gen:const (list co))
+                                             #f))))]
+      ['unbox         (cons 'unbox (TFun (λ (co) (gen:const (list (TBox co))))))]
+      ['box?          (cons 'box? (TFun (basic-fun (TBool) (TAny))))]))
   (map lookup-op ops))
 
 (define (build-gen:expr #:values vts #:forms fs)
@@ -453,8 +477,113 @@
 (define box-ops
   '(box unbox box?))
 
-#;(define arith-lang
+(define arith-lang
     ((build-gen:expr #:values (list 'integers 'booleans)
                      #:forms (list 'app 'if 'let 'let* 'cond))
      (build-env (list 'add1 'sub1 '+ '- 'zero? 'not))
      (TAny)))
+
+#|
+(begin-for-syntax
+  
+  (define-syntax-class value-type
+    (pattern (~or* (~datum 'integers)
+                   (~datum 'booleans)
+                   (~datum 'characters)
+                   (~datum 'eof))))
+
+  (define-syntax-class expr-form
+    (pattern (~or* (~datum 'app)
+                   (~datum 'if)
+                   (~datum 'cond)
+                   (~datum 'case)
+                   (~datum 'let1)
+                   (~datum 'let)
+                   (~datum 'let*)
+                   (~datum 'begin))))
+
+  (define-syntax-class expr-op
+    (pattern (~or* (~datum 'add1) (~datum 'sub1) (~datum 'abs)
+                   (~datum 'not) (~datum 'zero?)
+                   (~datum 'integer?) (~datum 'boolean?) (~datum 'char?)
+                   (~datum 'eof-object?)
+                   (~datum 'unary-) (~datum 'binary-) (~datum 'un/binary-)
+                   (~datum 'binary+) (~datum '+)
+                   (~datum 'integer->char) (~datum 'char->integer)))))
+
+(define-syntax (lookup-op stx)
+  (syntax-parse stx
+    [(_ (~datum 'add1)) #'(cons 'add1 (T-> (TInt) (TInt)))]
+    [(_ (~datum 'sub1)) #'(cons 'sub1 (T-> (TInt) (TInt)))]
+    [(_ (~datum 'abs)) #'(cons 'abs (T-> (TInt) (TInt)))]
+    [(_ (~datum 'not)) #'(cons 'not (T-> (TAny) (TBool)))]
+    [(_ (~datum 'zero?)) #'(cons 'zero? (T-> (TInt) (TBool)))]
+    [(_ (~datum 'integer?)) #'(cons 'integer? (T-> (TAny) (TBool)))]
+    [(_ (~datum 'boolean?)) #'(cons 'boolean? (T-> (TAny) (TBool)))]
+    [(_ (~datum 'char?)) #'(cons 'char? (T-> (TAny) (TBool)))]
+    [(_ (~datum 'eof-object?)) #'(cons 'eof-object? (T-> (TAny) (TBool)))]
+    [(_ (~datum 'unary-)) #'(cons '- (T-> (TInt) (TInt)))]
+    [(_ (~datum 'binary-)) #'(cons '- (T-> (TInt) (TInt) (TInt)))]
+    [(_ (~datum 'un/binary-)) #'(cons '- (T->* (list (TInt)) (list (TInt)) #f (TInt)))]
+    [(_ (~datum 'binary+)) #'(cons '+ (T-> (TInt) (TInt) (TInt)))]
+    [(_ (~datum '+)) #'(cons '+ (T->* '() '() (TInt) (TInt)))]
+    [(_ (~datum 'integer->char)) #'(cons 'integer->char (T-> (TInt) (TChar)))]
+    [(_ (~datum 'char->integer)) #'(cons 'char->integer (T-> (TChar) (TInt)))]))
+
+(define-syntax (lookup-type stx)
+  (syntax-parse stx
+    [(_ (~datum 'integers)) #'(TInt)]
+    [(_ (~datum 'booleans)) #'(TBool)]
+    [(_ (~datum 'characters)) #'(TChar)]
+    [(_ (~datum 'eof)) #'(TEOF)]))
+
+(define-syntax (lookup-form-freq stx)
+  (syntax-parse stx
+    [(_ size (~datum 'app)) #'(* 2 size)]
+    [(_ size (~datum 'if)) #'(quotient size 2)]
+    [(_ size (~datum 'let1)) #'size]
+    [(_ size (~datum 'let)) #'(quotient size 2)]
+    [(_ size (~datum 'let*)) #'(quotient size 2)]
+    [(_ size (~datum 'cond)) #'(quotient size 4)]
+    [(_ size (~datum 'case)) #'(quotient size 4)]
+    [(_ size (~datum 'begin)) #'(quotient size 2)]))
+  
+(define-syntax (lookup-form-gen stx)
+  (syntax-parse stx
+    [(_ (~datum 'app)) #'gen:app]
+    [(_ (~datum 'if)) #'gen:if]
+    [(_ (~datum 'let1)) #'gen:let1-exp]
+    [(_ (~datum 'let)) #'gen:let-exp]
+    [(_ (~datum 'let*)) #'gen:let*-exp]
+    [(_ (~datum 'cond)) #'gen:cond]
+    [(_ (~datum 'case)) #'gen:case]
+    [(_ (~datum 'begin)) #'gen:begin]))
+
+(define-syntax-rule (make-gen:expr k fs ...)
+  (lambda (env type)
+    (gen:no-shrink
+     (gen:sized
+      (lambda (size)
+        (gen:frequency
+         (cons (cons 1 (gen:simple-expr k env type))
+               (list (cons (lookup-form-freq size fs)
+                           ((lookup-form-gen fs) k env type))
+                     ...))))))))
+
+;; define a lambda that takes an env and a type
+;; inside that lambda is a letrec that defines gen:expr and all the forms
+;; also defines gen:base-type and gen:val based on #:values
+
+;; TODO: errors for incompatible choices
+(define-syntax (lang-generator stx)
+  (syntax-parse stx
+    [(_ #:values vts:value-type ...+
+        #:forms fs:expr-form ...
+        #:ops ops:expr-op ...)
+     #'(lambda (initial-env type)
+         (letrec ([gen:base-type (gen:one-of (list (lookup-type vts) ...))]
+                  [gen:expr (make-gen:expr k fs ...)]
+                  [k (knot gen:expr gen:base-type)])
+           (gen:expr (append initial-env (list (lookup-op ops) ...)) type)))]))
+|#
+    
